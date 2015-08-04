fb6a4f5437598d037531b18397e03e3906ca4a33
Daniel Amarante <daniel.amarante2@gmail.com>
2015-08-04 15:08:38 -0300

    removes sks

--- FinalChallenge/FinalChallenge/FlappyGameScene.swift
+++ FinalChallenge/FinalChallenge/FlappyGameScene.swift
@@ -10,5 +10,25 @@ import Foundation
 import SpriteKit
 
 class FlappyGameScene : SKScene, SKPhysicsContactDelegate {
-    
+    override func didMoveToView(view: SKView) {
+        
+        // setup physics
+        self.physicsWorld.gravity = CGVectorMake( 0.0, -5.0 )
+        self.physicsWorld.contactDelegate = self
+        
+        // setup background color
+        var skyColor = SKColor(red: 81.0/255.0, green: 192.0/255.0, blue: 201.0/255.0, alpha: 1.0)
+        self.backgroundColor = skyColor
+        
+        let birdTexture = SKTexture(imageNamed: "bird-02")
+        var bird = SKSpriteNode(texture: birdTexture)
+        bird.setScale(4.0)
+        bird.position = CGPoint(x: self.frame.size.width * 0.35, y:self.frame.size.height * 0.6)
+        
+        bird.physicsBody = SKPhysicsBody(circleOfRadius: bird.size.height / 2.0)
+        bird.physicsBody?.dynamic = true
+        bird.physicsBody?.allowsRotation = false
+        
+        self.addChild(bird)
+    }
 }
