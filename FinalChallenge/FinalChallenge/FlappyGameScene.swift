273831fd3e7c0ec997e238af46d8acf9ccf242d7
Daniel Amarante <daniel.amarante2@gmail.com>
2015-08-04 14:13:41 -0300

    flappy game classes added

---
+++ FinalChallenge/FinalChallenge/FlappyGameScene.swift
@@ -0,0 +1,14 @@
+//
+//  FlappyGameScene.swift
+//  FinalChallenge
+//
+//  Created by Daniel Amarante on 8/4/15.
+//  Copyright (c) 2015 Hariel Giacomuzzi. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class FlappyGameScene : SKScene, SKPhysicsContactDelegate {
+    
+}
---
+++ FinalChallenge/FinalChallenge/FlappyGameViewController.swift
@@ -0,0 +1,67 @@
+//
+//  FlappyGameViewController.swift
+//  FinalChallenge
+//
+//  Created by Daniel Amarante on 8/4/15.
+//  Copyright (c) 2015 Hariel Giacomuzzi. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+extension SKNode {
+    class func unarchiveFromFile(file : NSString) -> SKNode? {
+        
+        let path = NSBundle.mainBundle().pathForResource(file as String, ofType: "sks")
+        
+        let sceneData = NSData(contentsOfFile: path!, options: .DataReadingMappedIfSafe, error: nil)
+        let archiver = NSKeyedUnarchiver(forReadingWithData: sceneData!)
+        
+        archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+        let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! FlappyGameScene
+        archiver.finishDecoding()
+        return scene
+    }
+}
+
+class FlappyGameViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let scene = FlappyGameScene.unarchiveFromFile("FlappyScene") as? FlappyGameScene {
+            // Configure the view.
+            let skView = self.view as! SKView
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            skView.ignoresSiblingOrder = true
+            
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .AspectFill
+            
+            skView.presentScene(scene)
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
