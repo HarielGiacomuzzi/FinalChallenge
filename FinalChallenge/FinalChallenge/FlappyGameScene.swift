e84bcd40ac1df97d503ec59633d5088b2123b40c
brunork26 <bruno.r.kieling@hotmail.com>
2015-08-05 14:44:23 -0300

    pipes func added

--- FinalChallenge/FinalChallenge/FlappyGameScene.swift
+++ FinalChallenge/FinalChallenge/FlappyGameScene.swift
@@ -12,11 +12,19 @@ import SpriteKit
 class FlappyGameScene : SKScene, SKPhysicsContactDelegate {
     
     var player:SKSpriteNode!
-    
+    let verticalPipeGap = 150.0
+
     let playerCategory: UInt32 = 1 << 0
     let worldCategory: UInt32 = 1 << 1
     let pipeCategory: UInt32 = 1 << 2
     let scoreCategory: UInt32 = 1 << 3
+    
+    var pipeTextureUp:SKTexture!
+    var pipeTextureDown:SKTexture!
+    var movePipesAndRemove:SKAction!
+    var moving:SKNode!
+    var pipes:SKNode!
+    
     
     override func didMoveToView(view: SKView) {
         
@@ -27,6 +35,11 @@ class FlappyGameScene : SKScene, SKPhysicsContactDelegate {
         // setup background color
         var skyColor = SKColor(red: 81.0/255.0, green: 192.0/255.0, blue: 201.0/255.0, alpha: 1.0)
         self.backgroundColor = skyColor
+        
+        moving = SKNode()
+        self.addChild(moving)
+        pipes = SKNode()
+        moving.addChild(pipes)
         
         //draws player
         let playerTexture = SKTexture(imageNamed: "bird-02")
@@ -61,7 +74,8 @@ class FlappyGameScene : SKScene, SKPhysicsContactDelegate {
             // draws sprite
             let sprite = SKSpriteNode(texture: groundTexture)
             sprite.setScale(2.0)
-            sprite.position = CGPointMake(i * sprite.size.width, sprite.size.height / 2) // sprite.position = CENTER POINT
+            sprite.position = CGPointMake(i * sprite.size.width, sprite.size.height / 2)
+            // sprite.position = CENTER POINT
             
             //sets up movement
             sprite.runAction(moveGroundSpritesForever)
@@ -78,8 +92,73 @@ class FlappyGameScene : SKScene, SKPhysicsContactDelegate {
         ground.physicsBody?.categoryBitMask = worldCategory
         self.addChild(ground)
         
+        
+        // create the pipes textures
+        pipeTextureUp = SKTexture(imageNamed: "PipeUp")
+        pipeTextureUp.filteringMode = .Nearest
+        pipeTextureDown = SKTexture(imageNamed: "PipeDown")
+        pipeTextureDown.filteringMode = .Nearest
+        
+        // create the pipes movement actions
+        let distanceToMove = CGFloat(self.frame.size.width + 2.0 * pipeTextureUp.size().width)
+        let movePipes = SKAction.moveByX(-distanceToMove, y:0.0, duration:NSTimeInterval(0.01 * distanceToMove))
+        let removePipes = SKAction.removeFromParent()
+        movePipesAndRemove = SKAction.sequence([movePipes, removePipes])
+        
+        // spawn the pipes
+        let spawn = SKAction.runBlock({() in self.spawnPipes()})
+        let delay = SKAction.waitForDuration(NSTimeInterval(2.0))
+        let spawnThenDelay = SKAction.sequence([spawn, delay])
+        let spawnThenDelayForever = SKAction.repeatActionForever(spawnThenDelay)
+        self.runAction(spawnThenDelayForever)
     }
     
+    
+    func spawnPipes() {
+        let pipePair = SKNode()
+        pipePair.position = CGPointMake( self.frame.size.width + pipeTextureUp.size().width * 2, 0 )
+        pipePair.zPosition = -10
+        
+        let height = UInt32( UInt(self.frame.size.height / 4) )
+        let y = arc4random() % height + height
+        
+        let pipeDown = SKSpriteNode(texture: pipeTextureDown)
+        pipeDown.setScale(2.0)
+        pipeDown.position = CGPointMake(0.0, CGFloat(Double(y)) + pipeDown.size.height + CGFloat(verticalPipeGap))
+        
+        
+        pipeDown.physicsBody = SKPhysicsBody(rectangleOfSize: pipeDown.size)
+        pipeDown.physicsBody?.dynamic = false
+        pipeDown.physicsBody?.categoryBitMask = pipeCategory
+        pipeDown.physicsBody?.contactTestBitMask = playerCategory
+        pipePair.addChild(pipeDown)
+        
+        let pipeUp = SKSpriteNode(texture: pipeTextureUp)
+        pipeUp.setScale(2.0)
+        pipeUp.position = CGPointMake(0.0, CGFloat(Double(y)))
+        
+        pipeUp.physicsBody = SKPhysicsBody(rectangleOfSize: pipeUp.size)
+        pipeUp.physicsBody?.dynamic = false
+        pipeUp.physicsBody?.categoryBitMask = pipeCategory
+        pipeUp.physicsBody?.contactTestBitMask = playerCategory
+        pipePair.addChild(pipeUp)
+        
+        var contactNode = SKNode()
+        contactNode.position = CGPointMake( pipeDown.size.width + player.size.width / 2, CGRectGetMidY( self.frame ) )
+        contactNode.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake( pipeUp.size.width, self.frame.size.height ))
+        contactNode.physicsBody?.dynamic = false
+        contactNode.physicsBody?.categoryBitMask = scoreCategory
+        contactNode.physicsBody?.contactTestBitMask = playerCategory
+        pipePair.addChild(contactNode)
+        
+        pipePair.runAction(movePipesAndRemove)
+        pipes.addChild(pipePair)
+        
+    }
+    
+    func sendDataToOtherPlayers(){
+        
+    }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
         for touch: AnyObject in touches {
@@ -89,14 +168,6 @@ class FlappyGameScene : SKScene, SKPhysicsContactDelegate {
             player.physicsBody?.applyImpulse(CGVectorMake(0, 30))
                 
         }
-    }
-    
-    func sendDataToOtherPlayers(){
-        
-    }
-    
-    func getDataFromPlayer(data:NSNotification){
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: "messageReceived:", name: "ConnectionManager_DataReceived", object: nil)
     }
     
 }
