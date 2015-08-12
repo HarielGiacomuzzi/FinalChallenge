020dea2cee6470a84c555a28e26077bf1d6d3c54
Hariel Giacomuzzi <hariel.dias@acad.pucrs.br>
2015-08-12 14:06:32 -0300

    board init

---
+++ FinalChallenge/FinalChallenge/BoardController.swift
@@ -0,0 +1,14 @@
+//
+//  BoardController.swift
+//  FinalChallenge
+//
+//  Created by Hariel Giacomuzzi on 8/5/15.
+//  Copyright (c) 2015 Hariel Giacomuzzi. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class BoardController: SKScene, SKPhysicsContactDelegate {
+
+}
---
+++ FinalChallenge/FinalChallenge/BoardViewController.swift
@@ -0,0 +1,64 @@
+//
+//  BoardViewController.swift
+//  FinalChallenge
+//
+//  Created by Hariel Giacomuzzi on 8/5/15.
+//  Copyright (c) 2015 Hariel Giacomuzzi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SpriteKit
+
+extension SKNode {
+    class func unarchiveFromFile(file : NSString) -> SKNode? {
+        
+        let path = NSBundle.mainBundle().pathForResource(file as String, ofType: "sks")
+        
+        let sceneData = NSData(contentsOfFile: path!, options: .DataReadingMappedIfSafe, error: nil)
+        let archiver = NSKeyedUnarchiver(forReadingWithData: sceneData!)
+        
+        archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
+        let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as! BoardController
+        archiver.finishDecoding()
+        return scene
+    }
+}
+
+class BoardViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let scene = BoardController.unarchiveFromFile("Board") as? BoardController {
+            // Configure the view.
+            let skView = self.view as! SKView
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            skView.ignoresSiblingOrder = true
+            
+            /* Set the scale mode to scale to fit the window */
+            scene.scaleMode = .AspectFill
+            
+            skView.presentScene(scene)
+        }
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+    
+    override func supportedInterfaceOrientations() -> Int {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+            return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
+        } else {
+            return Int(UIInterfaceOrientationMask.All.rawValue)
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+}
