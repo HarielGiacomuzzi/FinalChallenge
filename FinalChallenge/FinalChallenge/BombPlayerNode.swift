8dba830227c1eff128b26ac13503193d27dc0cde
Daniel Amarante <daniel.amarante2@gmail.com>
2015-08-26 16:20:24 -0300

    merge

--- FinalChallenge/FinalChallenge/BombPlayerNode.swift
+++ FinalChallenge/FinalChallenge/BombPlayerNode.swift
@@ -15,13 +15,11 @@ class BombPlayerNode: SKSpriteNode {
     let worldCategory: UInt32 = 1 << 1
     let bombCategory: UInt32 = 1 << 2
     
+    var roboBody : SKSpriteNode?
     var identifier = ""
     
-    init() {
-        super.init(texture: nil, color: UIColor.blueColor(), size: CGSize(width: 55   , height: 60))
-    }
     
-    init(pos:BombTGameScene.Position,frame:CGRect) {
+    init() {
         
         let spriteAnimatedAtlas = SKTextureAtlas(named: "bombGame")//sprites
         
@@ -36,7 +34,6 @@ class BombPlayerNode: SKSpriteNode {
         
         super.init(texture: runFrames[0], color: UIColor.blueColor(), size: runFrames[0].size())
         setupPhysics()
-        setupMovement(pos,frame: frame)
         
         var animationAction = SKAction.animateWithTextures(runFrames, timePerFrame: 0.15)
         self.runAction(SKAction.repeatActionForever(animationAction))
@@ -58,33 +55,7 @@ class BombPlayerNode: SKSpriteNode {
         self.physicsBody?.collisionBitMask = worldCategory
         self.physicsBody?.contactTestBitMask = bombCategory
     }
-    
-    func setupMovement(pos:BombTGameScene.Position, frame:CGRect) {
-        switch pos {
-        case .North:
-            self.position = CGPointMake(frame.size.width/2 - frame.size.width/3.5, frame.size.height - 140)
-            let playerMovementDir = SKAction.moveTo(CGPointMake(frame.size.width/2 + frame.size.width/3.5, self.position.y), duration: 3.5)
-            let playerMovementEsq = SKAction.moveTo(CGPointMake(frame.size.width/2 - frame.size.width/3.5, self.position.y), duration: 3.5)
-            self.runAction(SKAction.repeatActionForever(SKAction.sequence([playerMovementDir, playerMovementEsq])))
-        case .South:
-            self.position = CGPointMake(frame.size.width/2 + frame.size.width/3.5, 140)
-            let playerMovementDir = SKAction.moveTo(CGPointMake(frame.size.width/2 + frame.size.width/3.5, self.position.y), duration: 3.5)
-            let playerMovementEsq = SKAction.moveTo(CGPointMake(frame.size.width/2 - frame.size.width/3.5, self.position.y), duration: 3.5)
-            self.runAction(SKAction.repeatActionForever(SKAction.sequence([playerMovementEsq, playerMovementDir])))
-        case .East:
-            self.position = CGPointMake(frame.size.width/2 + frame.size.width/3.5, frame.size.height - 140)
-            let playerMovementUp = SKAction.moveTo(CGPointMake(frame.size.width/2 + frame.size.width/3.5, frame.size.height - 140), duration: 3.5)
-            let playerMovementDown = SKAction.moveTo(CGPointMake(frame.size.width/2 + frame.size.width/3.5, 140), duration: 3.5)
-            self.runAction(SKAction.repeatActionForever(SKAction.sequence([playerMovementDown, playerMovementUp])))
-        case .West:
-            self.position = CGPointMake(frame.size.width/2 - frame.size.width/3.5, 140)
-            let playerMovementUp = SKAction.moveTo(CGPointMake(frame.size.width/2 - frame.size.width/3.5, frame.size.height - 140), duration: 3.5)
-            let playerMovementDown = SKAction.moveTo(CGPointMake(frame.size.width/2 - frame.size.width/3.5, 140), duration: 3.5)
-            self.runAction(SKAction.repeatActionForever(SKAction.sequence([playerMovementUp, playerMovementDown])))
-        default:
-            ()
-        }
-    }
+
     
     func initiateRoboBody(){
         
