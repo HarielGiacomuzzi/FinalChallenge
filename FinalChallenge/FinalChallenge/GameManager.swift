eea60c5d26b439137e26bf5c7554406ba4f95214
Hariel Giacomuzzi <hariel.dias@acad.pucrs.br>
2015-08-26 14:56:47 -0300

    player turns beta

--- FinalChallenge/FinalChallenge/GameManager.swift
+++ FinalChallenge/FinalChallenge/GameManager.swift
@@ -7,17 +7,39 @@
 //
 
 import Foundation
+import UIKit
 
 class GameManager {
     static let sharedInstance = GameManager();
     var gameActive = String()
+    var boardViewController : UIViewController?
     var playerRank = [String]()
     var isMultiplayer : Bool?
     var players = [Player]()
     
     init(){
-            
+            NSNotificationCenter.defaultCenter().addObserver(self, selector: "messageReceived:", name: "ConnectionManager_DataReceived", object: nil);
     }
+    
+    func playerTurnEnded(player : Player){
+      //chama aqui o próximo player :D 
+    }
+    
+    func messageReceived(data : NSNotification){
+        if let message = NSKeyedUnarchiver.unarchiveObjectWithData(data.object as! NSData) as? NSDictionary{
+            if message.valueForKey("diceResult") != nil {
+                var diceResult = message.valueForKey("diceResult") as! Int;
+                for p in players{
+                    if p.playerIdentifier == (message.valueForKey("playerID") as! String){
+                        BoardGraph.SharedInstance.walk(diceResult, player: p, view: boardViewController);
+                        playerTurnEnded(p)
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    
     
     func setPlayerOrder()->[String]{
         return playerRank.reverse()
