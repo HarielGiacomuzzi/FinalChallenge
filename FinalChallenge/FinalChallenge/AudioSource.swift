9e7722e5820c38b82e64f3ef5c973f7e5eb288a4
brunork26 <bruno.r.kieling@hotmail.com>
2015-08-27 16:13:50 -0300

    gitMayExplode

---
+++ FinalChallenge/FinalChallenge/AudioSource.swift
@@ -0,0 +1,64 @@
+//
+//  AudioSource.swift
+//  FinalChallenge
+//
+//  Created by bruno raupp kieling on 8/27/15.
+//  Copyright (c) 2015 Hariel Giacomuzzi. All rights reserved.
+//
+
+import AVFoundation
+import SpriteKit
+
+class AudioSource: NSObject, AVAudioPlayerDelegate {
+    
+    var soundTimer : NSTimer = NSTimer()
+    var url : NSURL!
+    var avPlayer : AVAudioPlayer!
+    var error : NSError?
+    var bubbleSound : SKAction!
+    static let sharedInstance = AudioSource()
+    
+    override init(){
+        super.init()
+    }
+    
+    func playBubbleSound() -> SKAction{
+         bubbleSound = SKAction.playSoundFileNamed("bubbleSound2.wav", waitForCompletion: false)
+        return bubbleSound
+        //readAudioFile("bubbleSound2", ext: "wav", audioLoop: false)
+        //readAudioFile("explosionSound", ext: "wav", audioLoop: false)
+    }
+    
+    func flappyFishSound(){
+        readAudioFile("funMusic", ext: "mp3", audioLoop: true)
+    }
+    
+    func readAudioFile(fileName:String, ext:String, audioLoop:Bool){ // prepara audio
+        self.url = NSBundle.mainBundle().URLForResource(fileName, withExtension: ext)
+        self.avPlayer = AVAudioPlayer(contentsOfURL: self.url, error: &error)
+        if avPlayer == nil {
+            if let e = error {
+                println(e.localizedDescription)
+            }
+        }
+        
+        avPlayer.delegate = self
+        avPlayer.volume = 1.0
+        if audioLoop == true { // this loops the audio, like ambient audio, or music
+            soundTimer = NSTimer.scheduledTimerWithTimeInterval(-1, target: self, selector: Selector("soundTimerPlayed"), userInfo: nil, repeats: true)
+        } else {
+            self.soundTimerPlayed()
+        }
+        
+    }
+    
+    func soundTimerPlayed(){
+        avPlayer.prepareToPlay()
+        avPlayer.play()
+    }
+    
+    func stopAudio(){
+        avPlayer.stop()
+    }
+    
+}
