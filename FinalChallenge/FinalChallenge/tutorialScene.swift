f3c5a3cb22fef335161d4d14f775079e57197c3c
brunork26 <bruno.r.kieling@hotmail.com>
2015-09-25 13:39:06 -0300

    tutorial scene sprite action working well now

--- FinalChallenge/FinalChallenge/tutorialScene.swift
+++ FinalChallenge/FinalChallenge/tutorialScene.swift
@@ -182,7 +182,7 @@ class tutorialScene : SKScene, SKPhysicsContactDelegate {
                 }
                 
                 
-                _ = SKAction.animateWithTextures(runFrames, timePerFrame: 0.05)
+                let firstAction = SKAction.animateWithTextures(runFrames, timePerFrame: 0.05)
                 
                 
                 let label : SKLabelNode = SKLabelNode(text: sprite.name)
@@ -196,9 +196,9 @@ class tutorialScene : SKScene, SKPhysicsContactDelegate {
                 square.zPosition = 100
                 square.position = CGPoint(x: label.position.x, y: label.position.y  + 5)
                 self.addChild(square)
-                //square.runAction(firstAction, completion: { () -> Void in
-                //    self.addChild(label)
-                //})
+                square.runAction(firstAction, completion: { () -> Void in
+                    self.addChild(label)
+                })
                 
                 
             }
