b308a856dd52ab5fbb42b6c4392a45087e14f43a
Daniel Amarante <daniel.amarante2@gmail.com>
2015-10-06 16:47:50 -0300

    esse commit deixa o dice result sempre 20 nao vou dar push so quero dar ull

--- FinalChallenge/FinalChallenge/PlayerControllerScene.swift
+++ FinalChallenge/FinalChallenge/PlayerControllerScene.swift
@@ -164,13 +164,15 @@ class PlayerControllerScene: SKScene, CardCarousellDelegate {
         addChild(testButton)
     }
     
+    
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         for touch: AnyObject in touches {
             
             let location = touch.locationInNode(self)
             if testButton.containsPoint(location) {
                 if testButton.text == "DICE" {
-                    let diceResult = Int(arc4random_uniform(6)+1)
+//                    let diceResult = Int(arc4random_uniform(6)+1)
+                    let diceResult = 20
                     let aux = NSMutableDictionary();
                     aux.setValue(diceResult, forKey: "diceResult");
                     aux.setValue(ConnectionManager.sharedInstance.peerID!.displayName, forKey: "playerID");
