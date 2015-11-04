2cf8b32ae57043a33da957acb471f05313a887df
Jonathas Hernandes <joohernandes@gmail.com>
2015-11-04 17:14:04 -0200

    tela mindingo atualizada. so falta acertar os valores das labels

--- FinalChallenge/FinalChallenge/iPhonePlayerViewController.swift
+++ FinalChallenge/FinalChallenge/iPhonePlayerViewController.swift
@@ -46,10 +46,10 @@ class iPhonePlayerViewController: UIViewController {
         skView?.showsNodeCount = false
         skView?.ignoresSiblingOrder = true
         skView?.showsPhysics = false
-        playerCards = ["StealGoldCard","LoseCard","MoveBackCard","Skull","Lamp"]
- //     loadPartyModeScene()
- //       loadStore(["StealGoldCard","StealGoldCard","StealGoldCard","MoveBackCard","LoseCard"])
-        loadPlayerView()
+  //      playerCards = ["StealGoldCard","LoseCard","MoveBackCard","Skull","Lamp"]
+       // loadPartyModeScene()
+        loadStore(["StealGoldCard","StealGoldCard","StealGoldCard","MoveBackCard","LoseCard"])
+        //loadPlayerView()
     }
     
     // MARK: - Message Received Functions
